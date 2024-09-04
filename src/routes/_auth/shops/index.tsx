914373c6711c604df1108b5b6683013648789fc8
Will Trojniak <william.trojniak@gmail.com>
2024-09-04 18:58:38 -0400

    Cleanup shop landing page

--- src/routes/_auth/shops/index.tsx
+++ src/routes/_auth/shops/index.tsx
@@ -5,6 +5,9 @@ import { ShopFormDialog } from '@/components/forms/shop-form';
 import { PaymentMethod } from '@/types/types';
 import { CreateButton } from '@/components/ui/create-button';
 import { useAuth } from '@/providers/auth';
+import { Card, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { ExternalLink } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export const Route = createFileRoute('/_auth/shops/')({
   component: ShopsComponent,
@@ -16,11 +19,18 @@ export const Route = createFileRoute('/_auth/shops/')({
 function ShopsComponent() {
   const { user } = useAuth();
   const { data } = useSuspenseQuery(getShopsForUserIdQueryOptions(user!.id))
-  return <div className='flex flex-col items-start gap-4 max-w-full'>
-    <ShopFormDialog paymentMethods={[PaymentMethod.in_person, PaymentMethod.chartstring]}>
-      <CreateButton>Create Shop</CreateButton>
-    </ShopFormDialog>
+  return <div className='flex flex-col items-center gap-4 max-w-full'>
+    {data.length === 0 &&
+      <ShopFormDialog paymentMethods={[PaymentMethod.in_person, PaymentMethod.chartstring]}>
+        <CreateButton>Create Shop</CreateButton>
+      </ShopFormDialog>
+    }
     {data.length === 0 && "No shops to display. Create one to get started."}
-    {data.map((e) => <Link key={e.id} to="/shops/$shopId" params={{ shopId: e.id }}>{e.name}</Link>)}
+    <div className='flex flex-row flex-wrap gap-4'>
+      {data.map((shop) => <Card key={shop.id}>
+        <CardHeader><CardTitle>{shop.name}</CardTitle></CardHeader>
+        <CardFooter><Link to='/shops/$shopId' params={{ shopId: shop.id }}><Button className='gap-2'>Go to shop<ExternalLink className='w-4 h-4' /></Button></Link></CardFooter>
+      </Card>)}
+    </div>
   </div>
 }
